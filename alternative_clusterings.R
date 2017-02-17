cb58376afa810bf4703286cfa39e9227932d8cf1
Tamar Loach <tamar@Tamars-MacBook-Air.local>
2017-02-17 15:54:03 +0000

    new method

--- alternative_clusterings.R
+++ alternative_clusterings.R
@@ -4,3 +4,5 @@ dim(genotypesImputed)
 heatmap(genotypesImputed, scale = "none")
 km = kmeans(genotypesImputed,11)
 
+d = dist(genotypesImputed, method="binary")
+hc = data.frame(cutree(hclust(d, method="average"), k=11))
