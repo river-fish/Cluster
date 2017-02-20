9dc85353eb1ced9963ce7645ba45af8cd1a5669b
EmiliaPompe <pompe.emilia@gmail.com>
2017-02-20 10:15:20 +0000

    alt

--- alternative_clusterings.R
+++ alternative_clusterings.R
@@ -26,30 +26,69 @@ k <- length(table(summary(df1$cluster_id)))
 print(k)
 
 
-ConcordanceFunction<- function(df1, df2){
- 
-  k=11
-  concordances_in_clusters <- sapply(1:k, function(i){
-    cluster_patient_ids <- df1[df1$cluster_id==i,]$patient_id
-    df_temp <- df2[df2$patient_id %in% cluster_patient_ids,]
-    people_together <- unname(summary(as.factor(df_temp$cluster_id)))
-    sum(sapply(people_together, function(n) choose(n,2)))
+# ConcordanceFunction_ver2<- function(df1, df2){
+#  
+#   k=11
+#   concordances_in_clusters <- sapply(1:k, function(i){
+#     cluster_patient_ids <- df1[df1$cluster_id==i,]$patient_id
+#     df_temp <- df2[df2$patient_id %in% cluster_patient_ids,]
+#     people_together <- unname(summary(as.factor(df_temp$cluster_id)))
+#     sum(sapply(people_together, function(n) choose(n,2)))
+#   })
+#   return(sum(concordances_in_clusters))
+# }
+# 
+# ConcordanceFunction_ver2(df2,df1)
+# 
+# summary(as.factor(df1$cluster_id))
+
+# N <- 100
+# for (i in 1:2){
+#   genotypesImputed_boot <- genotypesImputed[sample(1:nrow(genotypesImputed), size = nrow(genotypesImputed), replace = TRUE),]
+#   res <- hierarchical(genotypesImputed_boot)
+#   res[,2] <- rownames(genotypesImputed_boot)
+#   colnames(res) = c(paste0('clusterization_', i), 'patient_id')
+#   df <- merge(df, res)
+# }
+
+
+ConcordanceFunction <- function(df1, df2){
+  if(nrow(df1)!=nrow(df2)){
+    stop('The number of rows in the data frames are not equal')
+  }
+  df2_ordered <- df2[rownames(df1),]
+  counts <- sapply(seq_len(nrow(df1)), function(k){
+    count = 0
+    for (i in seq_len(k-1)){
+      if((df1$cluster_id[k] == df1$cluster[i]) & (df2_ordered$cluster_id[k] == df2_ordered$cluster[i])) count = count +1
+      else if ((df1$cluster_id[k] != df1$cluster[i]) & (df2_ordered$cluster_id[k] != df2_ordered$cluster[i])) count = count +1
+    }
+    return(count)
   })
-  return(sum(concordances_in_clusters))
+  return(sum(counts)/choose(nrow(df1),2))
 }
 
-ConcordanceFunction(df2,df1)
-
-summary(as.factor(df1$cluster_id))
-
-N <- 100
-for (i in 1:2){
-  genotypesImputed_boot <- genotypesImputed[sample(1:nrow(genotypesImputed), size = nrow(genotypesImputed), replace = TRUE),]
-  res <- hierarchical(genotypesImputed_boot)
-  res[,2] <- rownames(genotypesImputed_boot)
-  colnames(res) = c(paste0('clusterization_', i), 'patient_id')
-  df <- merge(df, res)
-}
+ConcordanceFunction(df1[1:1000,], df1[1:1000,])
 
 
 
+# ConcordanceFunctionF <- function(df1, df2){
+#   if(nrow(df1)!=nrow(df2)){
+#     stop('The number of rows in the data frames are not equal')
+#   }
+#   df2_ordered <- df2[rownames(df1),]
+#   counts <- sapply(2:nrow(df1), function(k){
+#     
+#     single_count <- sapply(seq_len(k-1), function(i) {
+#       if((df1$cluster_id[k] == df1$cluster[i]) & (df2_ordered$cluster_id[k] == df2_ordered$cluster[i])) return(1)
+#       else if ((df1$cluster_id[k] != df1$cluster[i]) & (df2_ordered$cluster_id[k] != df2_ordered$cluster[i])) return(1)
+#       else return(0)
+#     })
+#     return(sum(single_count))
+#   })
+#   return(sum(counts)/choose(nrow(df1),2))
+# }
+# 
+# 
+# system.time(ConcordanceFunctionF(df1[1:1000,], df1[1:1000,]))
+system.time(ConcordanceFunction(df1[1:1000,], df1[1:1000,]))
