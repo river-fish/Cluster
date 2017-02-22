792f0a155e3749ec6a6b9b9b6c147caa30376db9
Emilia Pompe <pompe@tocotoucan.stats.ox.ac.uk>
2017-02-22 16:26:57 +0000

    .

--- concordance_gibbs_sampler.R
+++ concordance_gibbs_sampler.R
@@ -1,49 +1,49 @@
-n_cores = 4
+n_cores = 6
+library(parallel)
+
+getwd()
 
 set.seed(1)
 
-X = mclapply(1:times,function(t){
+for (nr_clust in c(9)){
   
-  if (t==1) {
-    j = t
-    concordances_gibbs <- c()
-    
-    vec <- seq(from=j*1000, to = (j+1)*1000, by=100)
-    for(nr_gibb in vec[-1]){
-      
-      cluster_id <- gibbs_output[[nr_gibb-1]]$Z
-      df1 <- data.frame(cluster_id = cluster_id)
-      rownames(df1) <- rownames(genotypesImputed)
-     
-      cluster_id <- gibbs_output[[nr_gibb]]$Z
-      df2 <- data.frame(cluster_id = cluster_id)
-      rownames(df2) <- rownames(genotypesImputed)
-      
-      concordances_gibbs <- c(concordances_gibbs, ConcordanceFunction(df1, df2))
+  load(paste0('gibbs_result_part2_',nr_clust,'_50000.RData'))
+  gibbs_output <- result_part1
+  
+  
+  
+  start = Sys.time()
+  
+  X = mclapply(1:n_cores,function(t){
+    for(j in 1:n_cores){
+      if (t==j) {
+        
+        concordances_gibbs <- c()
+        
+        vec <- 1+seq(from=(j-1)*1000, to = (j)*1000, by=100)
+        for(nr_gibb in 2:length(vec)){
+          
+          cluster_id <- gibbs_output[[(vec[nr_gibb-1])]]$Z
+          df1 <- data.frame(cluster_id = cluster_id)
+          rownames(df1) <- rownames(genotypesImputed)
+          
+          cluster_id <- gibbs_output[[(vec[nr_gibb])]]$Z
+          df2 <- data.frame(cluster_id = cluster_id)
+          rownames(df2) <- rownames(genotypesImputed)
+          
+          concordances_gibbs <- c(concordances_gibbs, ConcordanceFunction(df1, df2))
+        }
+        
+        save(concordances_gibbs, file=paste0('concordances_gibbs_part2',j,'_nr_clust_',nr_clust,'.RData'))
+        return(concordances_gibbs)
+        
+      }
     }
     
-    save(concordances_gibbs, file=paste0('concordances_gibbs_',j,'.RData'))
-    return(concordances_gibbs)
-    
   }
   
-  if (t==2) {
-    
-    
-    return(list_b)
-  }
-  
-  if (t==3) {
-    
-    
-    
-    return(list_c)
-  }
-  if (t==4) {
-    
-    
-    
-    return(list_d)
-  }}
-  
-  ,mc.cores = ncores)
+  ,mc.cores = n_cores)
+}
+
+now = Sys.time()
+print(now - start)
--- gibbs_to_server.R
+++ gibbs_to_server.R
@@ -13,8 +13,65 @@ w = rep(1/K, times =K)
 Z <- sample(1:K, nrow(X), prob=w, replace = TRUE)
 theta <- rdirichlet(K, alpha)
 
-N <- 10000
+N <- 50000
 system.time(
-result <- FullGibbs(N, X = X, w_start = w, theta_start = theta, Z_start = Z, alpha = alpha, beta=beta)
+result_part1 <- FullGibbs(N, X = X, w_start = w, theta_start = theta, Z_start = Z, alpha = alpha, beta=beta)
 )
-save(result, file=paste0('gibbs_result_',N, '.RData'))
+save(result_part1, file=paste0('gibbs_result_part1_',K,'_',N, '.RData'))
+print('hello')
+
+system.time(
+  result_part2 <- FullGibbs(N, X = X, w_start = result_part1[[N]]$w, theta_start = result_part1[[N]]$theta, Z_start = result_part1[[N]]$Z, alpha = alpha, beta=beta)
+)
+save(result_part2, file=paste0('gibbs_result_part2_',K,'_',N, '.RData'))
+print('hello2')
+
+##################### 10 clusters
+m=84
+K = 10
+X <- genotypesImputed
+
+beta <- rep(1/K, times=K)
+alpha <- rep(1/m, times=m)
+w = rep(1/K, times =K)
+Z <- sample(1:K, nrow(X), prob=w, replace = TRUE)
+theta <- rdirichlet(K, alpha)
+
+N <- 50000
+system.time(
+  result_part1 <- FullGibbs(N, X = X, w_start = w, theta_start = theta, Z_start = Z, alpha = alpha, beta=beta)
+)
+save(result_part1, file=paste0('gibbs_result_part1_',K,'_',N, '.RData'))
+print('hello')
+
+system.time(
+  result_part2 <- FullGibbs(N, X = X, w_start = result_part1[[N]]$w, theta_start = result_part1[[N]]$theta, Z_start = result_part1[[N]]$Z, alpha = alpha, beta=beta)
+)
+save(result_part2, file=paste0('gibbs_result_part2_',K,'_',N, '.RData'))
+print('hello2')
+
+
+################## 9 clusters
+
+m=84
+K = 9
+X <- genotypesImputed
+
+beta <- rep(1/K, times=K)
+alpha <- rep(1/m, times=m)
+w = rep(1/K, times =K)
+Z <- sample(1:K, nrow(X), prob=w, replace = TRUE)
+theta <- rdirichlet(K, alpha)
+
+N <- 50000
+system.time(
+  result_part1 <- FullGibbs(N, X = X, w_start = w, theta_start = theta, Z_start = Z, alpha = alpha, beta=beta)
+)
+save(result_part1, file=paste0('gibbs_result_part1_',K,'_',N, '.RData'))
+print('hello')
+
+system.time(
+  result_part2 <- FullGibbs(N, X = X, w_start = result_part1[[N]]$w, theta_start = result_part1[[N]]$theta, Z_start = result_part1[[N]]$Z, alpha = alpha, beta=beta)
+)
+save(result_part2, file=paste0('gibbs_result_part2_',K,'_',N, '.RData'))
+print('hello2')
