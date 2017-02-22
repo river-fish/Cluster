215c63c6acefedabfdb64593883b6111e6b67524
Emilia Pompe <pompe@tocotoucan.stats.ox.ac.uk>
2017-02-22 17:47:01 +0000

    .

---
+++ plots.R
@@ -0,0 +1,53 @@
+
+nr_clust <- 11
+comparison_11 <- c()
+for(j in 1:6){
+  load(paste0('comparison_final_gibbs',j,'_nr_clust_',nr_clust,'.RData'))
+  comparison_11 <- c(comparison_11, concordances_gibbs)
+}
+
+hist(comparison_11)
+
+nr_clust <- 10
+comparison_10 <- c()
+for(j in 1:6){
+  load(paste0('comparison_final_gibbs',j,'_nr_clust_',nr_clust,'.RData'))
+  comparison_10 <- c(comparison_10, concordances_gibbs)
+}
+hist(comparison_10)
+
+
+nr_clust <- 9
+comparison_9 <- c()
+for(j in 1:6){
+  load(paste0('comparison_final_gibbs',j,'_nr_clust_',nr_clust,'.RData'))
+  comparison_9 <- c(comparison_9, concordances_gibbs)
+}
+hist(comparison_9)
+
+#-----------------
+load("~/Project7/Cluster/genotypesImputed.Rdata")
+source('Clust_Image.R')
+load("~/Project7/cl/gibbs_result_part2_10_50000.RData")
+load("Simply_cluster.Rdata")
+library(ggplot2)
+library(RColorBrewer)
+Cluster_Image(genotypesImputed, result_part2[[100]]$Z)
+Cluster_Image(genotypesImputed, result_part2[[1000]]$Z)
+Cluster_Image(genotypesImputed, result_part2[[10000]]$Z)
+Cluster_Image(genotypesImputed, result_part2[[7]]$Z)
+Cluster_Image(genotypesImputed, result_part2[[1300]]$Z)
+Cluster_Image(genotypesImputed, result_part2[[666]]$Z)
+
+
+Cluster_Image(genotypesImputed, as.numeric(as.character(dpClass)))
+ 
+              Impose_NPM1_Clust = T,
+              Impose_Fusion_clust=T,
+              Impose_CEBPA_bi =T,
+              Impose_IDH2_p172 =T,
+              Impose_Chromatin_Splice = T,
+              Impose_TP53_aneuploidy = TRUE)
+
+
+
